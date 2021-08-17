126f8022858bfe8382a5a1f0f21566a2382cc312
Sergey Osipov <47226965+devstratum@users.noreply.github.com>
2021-08-18 00:39:55 +0300

    Delete clearclass directory

--- clearclass/behavior.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-
-abstract class JHtmlBehavior
-{
-	public static function __callStatic($_name, $_param)
-	{
-	}
-}
--- clearclass/jquery.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-
-abstract class JHtmlJquery
-{
-	public static function __callStatic($_name, $_param)
-	{
-	}
-}
